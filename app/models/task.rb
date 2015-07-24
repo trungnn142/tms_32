7558dd1483f22008d35ee00d3b68107c35594f50
trungnn142 <trungnn142@gmail.com>
2015-07-24 16:25:21 +0700

    Finish created task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,10 @@
 class Task < ActiveRecord::Base
-  belongs_to :subject
+  belongs_to :subject, inverse_of: :tasks
   has_many :user_tasks
   has_many :users, through: :user_tasks
 
   validates :name, presence: true
   validates :order, presence: true, numericality: {only_integer: true,
     greater_than_or_equal_to: 0}
+  validates :subject, presence: true
 end
