21ba83b44bfc1597faf446b6bef45137563b28de
Trinh Duc Duy <trinh.duc.duy@framgia.com>
2015-07-28 10:27:21 +0700

    Separate add trainee and add supervisor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723041130) do
+ActiveRecord::Schema.define(version: 20150728061933) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "user_id"
@@ -104,5 +104,7 @@ ActiveRecord::Schema.define(version: 20150723041130) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  add_index "users", ["role"], name: "index_users_on_role"
 
 end
