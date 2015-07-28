0b510068f59040093ff6b9f865bc865136351b37
trungnn142 <trungnn142@gmail.com>
2015-07-28 13:58:07 +0700

    Finish add user to subject and modify model

--- app/models/user_subject.rb
+++ app/models/user_subject.rb
@@ -1,7 +1,11 @@
 class UserSubject < ActiveRecord::Base
   belongs_to :user
   belongs_to :subject
+  belongs_to :course
   has_many :user_tasks
 
   validates :user_id, uniqueness: {scope: :subject_id}
+
+  scope :filter_by_subject, ->subject_id{where subject_id: subject_id}
+  scope :filter_by_user, ->user_id{where user_id: user_id}
 end
--- app/models/user_task.rb
+++ app/models/user_task.rb
@@ -1,7 +1,6 @@
 class UserTask < ActiveRecord::Base
   belongs_to :user
   belongs_to :task
-  belongs_to :course
   belongs_to :user_subject
 
   validates :user_id, uniqueness: {scope: :task_id}
